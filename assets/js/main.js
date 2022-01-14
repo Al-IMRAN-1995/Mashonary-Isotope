593168b6372658d446e9286b6f5f5d4367fc8789
AL IMRAN <imu.png@gmail.com>
2022-01-15 05:41:53 +0600

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+(function ($) {
+    "use strict";
+
+
+// external js: isotope.pkgd.js
+
+$('.grid').isotope({
+  itemSelector: '.grid-item',
+  masonry: {
+    columnWidth: 100,
+  }
+});
+
+
+
+}(jQuery));
+
